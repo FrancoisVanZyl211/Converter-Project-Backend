207c7123495fc007ac063dbe8c9ec0953dc1f2ea
gerrit <gerrit.vanzyl@myob.com>
2023-04-02 19:34:01 +1200

    We changed Unit to Celcius/Fahrenheit and we added a new method that converts Fahrenheit to Celcius

--- ConverterProject-Backend/ConvertProject.API/Controllers/ConversionController.cs
+++ ConverterProject-Backend/ConvertProject.API/Controllers/ConversionController.cs
@@ -8,10 +8,17 @@ namespace ConvertProject.API.Controllers
     public class ConversionController : ControllerBase 
     {
         [HttpGet]
-        [Route("/conversion/ConvertCToF/{Unit}")]
-        public string ReturnResponse(double Unit)
+        [Route("/conversion/ConvertCToF/{Celcius}")]
+        public string ReturnCtoF(double Celcius)
         {
-            return ConversionLogic.ConvertCtoF(Unit).ToString();             
+            return ConversionLogic.ConvertCtoF(Celcius).ToString();
+        }
+
+        [HttpGet]
+        [Route("/conversion/ConvertFToC/{Fahrenheit}")]
+        public string ReturnFtoC(double Fahrenheit)
+        {
+            return ConversionLogic.ConvertFtoC(Fahrenheit).ToString();
         }
     }
 }
