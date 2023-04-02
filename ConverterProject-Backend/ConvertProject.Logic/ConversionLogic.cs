d7556a0ab58668e5e81943cd287d72c79d13cda4
gerrit <gerrit.vanzyl@myob.com>
2023-04-02 19:36:05 +1200

    We changed Unit to Celcius and we added a new method to Logic that Converts Fahrenheit to Celcius based on Logic

--- ConverterProject-Backend/ConvertProject.Logic/ConversionLogic.cs
+++ ConverterProject-Backend/ConvertProject.Logic/ConversionLogic.cs
@@ -2,9 +2,15 @@
 {
     public static class ConversionLogic
     {
-        public static double ConvertCtoF(double unit)
+        public static double ConvertCtoF(double Celcius)
         {
-            var convertedUnit = unit * 9 / 5 + 32;
+            var convertedUnit = Celcius * 9 / 5 + 32;
+            return convertedUnit;
+        }
+
+        public static double ConvertFtoC(double Fahrenheit)
+        {
+            var convertedUnit = (Fahrenheit - 32) * 5 / 9;
             return convertedUnit;
         }
     }
